3f7cdc2935840d2f0e33c5225619307a55c51e40
Max Miao <miaoma2@oocl.com>
2020-07-28 18:38:42 +0800

    [springboot-employee][max, front] fix-bug: remove redundant @service

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -1,11 +1,9 @@
 package com.thoughtworks.springbootemployee.service;
 
 import com.thoughtworks.springbootemployee.entity.Employee;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface EmployeeService {
     void addEmployee(Employee employee);
     void deleteEmployee(int id);

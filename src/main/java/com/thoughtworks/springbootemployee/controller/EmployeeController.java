54b1cdd9551c14bbbbac9e1f1de6564d8f005546
Max Miao <miaoma2@oocl.com>
2020-07-28 17:12:14 +0800

    [springbootemployee][max, front] feature : add getEmployee by id in EmployeeController

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        return new Employee(1,"2",2,"man+");
+    }
+}

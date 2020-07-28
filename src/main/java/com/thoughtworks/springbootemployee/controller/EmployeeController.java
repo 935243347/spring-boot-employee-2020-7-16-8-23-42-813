02226103173ff1f476de05398c04da49e387c986
Max Miao <miaoma2@oocl.com>
2020-07-28 18:40:23 +0800

    [springboot-employee][max, front] feature: add addEmployee api

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,18 +2,26 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/employees")
+    public void addEmployee(Employee employee){
+        employeeService.addEmployee(employee);
+    }
+
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id){
-        return new Employee(1,"2",2,"man+");
+        return employeeService.getEmployee(id);
     }
+
 }
